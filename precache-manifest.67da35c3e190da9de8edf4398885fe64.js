bfd49332d70bf50ebc5fb38b03d9500d46786726
Dzmitry Maridau <vinmaster@tut.by>
2020-01-02 01:47:15 +0300

    Updates

---
+++ precache-manifest.67da35c3e190da9de8edf4398885fe64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c05661b3e82cfb34ee27003d3935856",
+    "url": "/react-monsters-storage/index.html"
+  },
+  {
+    "revision": "89cd98e41b0b4e842936",
+    "url": "/react-monsters-storage/static/css/main.632bef7a.chunk.css"
+  },
+  {
+    "revision": "3e226d037caaae7579dc",
+    "url": "/react-monsters-storage/static/js/2.a6af99e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-storage/static/js/2.a6af99e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89cd98e41b0b4e842936",
+    "url": "/react-monsters-storage/static/js/main.51668464.chunk.js"
+  },
+  {
+    "revision": "fa528cb428e947602dc8",
+    "url": "/react-monsters-storage/static/js/runtime-main.44770149.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-monsters-storage/static/media/logo.5d5d9eef.svg"
+  }
+]);
